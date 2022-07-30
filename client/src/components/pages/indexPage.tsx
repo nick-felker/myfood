c5fb7d1ec5d41899540d00310a6db26006c49f27
Nikita Felker <companionofarthas@mail.ru>
2022-07-31 01:06:48 +0600

    init directories

---
+++ client/src/components/pages/indexPage.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+
+
+interface Props {
+
+}
+
+
+
+function IndexPage(props: Props) {
+    return (
+        <>
+
+
+        </>
+    )
+}
+
+
+export default IndexPage;
